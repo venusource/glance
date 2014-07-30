1450a7d265de30a287410ec428ae2112cdea1a30
lei.tang <lei.tang@venusource.com>
2014-07-30 13:42:58 +0800

    add scripts

--- setup-install.sh
+++ setup-install.sh
@@ -1,8 +1,8 @@
 INSTALL_ENV=`python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`
 [ -d ${INSTALL_ENV}/glance ] || mkdir -p ${INSTALL_ENV}/glance
 cp -rf /usr/share/glance/* ${INSTALL_ENV}/glance
-cp -rf /usr/local/etc/glance/rc.d/init.d/* /etc/init.d
-cp  /usr/local/etc/glance/rc.d/init.d/* /etc/rc.d/init.d/
+cp -rf /usr/local/etc/rc.d/init.d/openstack-glance* /etc/init.d
+cp  /usr/local/etc/rc.d/init.d/openstack-glance* /etc/rc.d/init.d/
 
 [ -d /etc/glance ] || mkdir /etc/glance
 cp /usr/share/glance/etc/* /etc/glance
@@ -11,3 +11,7 @@ cp /usr/local/*.py ${INSTALL_ENV}/glance/
 if [ -f /etc/glance/glance.conf.sample ];then
 mv -f /etc/glance/glance.conf.sample /etc/glance/glance.conf
 fi
+
+[ -d /var/lib/glance ] || mkdir /var/lib/glance
+[ -d /var/log/glance ] || mkdir /var/log/glance
+[ -d /var/run/glance ] || mkdir /var/run/glance
