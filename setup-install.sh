3a3b3aff077f78eb131408b7e6f44a1cb335b4ee
lei.tang <lei.tang@venusource.com>
2014-08-06 10:22:07 +0800

    modify the scripts of package

--- setup-install.sh
+++ setup-install.sh
@@ -1,17 +1,23 @@
 INSTALL_ENV=`python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`
 [ -d ${INSTALL_ENV}/glance ] || mkdir -p ${INSTALL_ENV}/glance
-cp -rf /usr/share/glance/* ${INSTALL_ENV}/glance
-cp -rf /usr/local/etc/rc.d/init.d/openstack-glance* /etc/init.d
-cp  /usr/local/etc/rc.d/init.d/openstack-glance* /etc/rc.d/init.d/
+cp -rf /usr/share/glance* ${INSTALL_ENV}/
+cp -rf /usr/local/etc/setup-scripts/openstack-glance* /etc/init.d
+cp  /usr/local/etc/setup-scripts/openstack-glance* /etc/rc.d/init.d/
 
 [ -d /etc/glance ] || mkdir /etc/glance
-cp /usr/share/glance/etc/* /etc/glance
-cp /usr/local/*.py ${INSTALL_ENV}/glance/
+cp /usr/local/etc/* /etc/glance
  
 if [ -f /etc/glance/glance.conf.sample ];then
 mv -f /etc/glance/glance.conf.sample /etc/glance/glance.conf
 fi
 
+groupadd -g 161 glance 2>/dev/null
+useradd -u 161 -g glance -c "OpenStack Glance Daemons" -d /var/lib/glance -s /sbin/nologin glance 2>/dev/null
+
 [ -d /var/lib/glance ] || mkdir /var/lib/glance
 [ -d /var/log/glance ] || mkdir /var/log/glance
 [ -d /var/run/glance ] || mkdir /var/run/glance
+
+chown -R glance:glance /var/lib/glance
+chown -R glance:glance /var/log/glance
+chown -R glance:glance /var/run/glance
